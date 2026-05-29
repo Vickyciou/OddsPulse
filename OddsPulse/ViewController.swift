70c3488dd49deeb18912806a2b01d40bd1cb87f5
VickyCiou <stu7032@gmail.com>
2026-05-29 11:58:04 +0800

    chore: initialize OddsPulse project
      - add the initial Xcode UIKit app scaffold
      - include unit and UI test targets
      - add project gitignore rules for Xcode-generated files

---
+++ OddsPulse/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OddsPulse
+//
+//  Created by Vickyciou on 2026/5/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
